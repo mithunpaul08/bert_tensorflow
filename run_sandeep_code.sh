8345a56f6ceeadad08a70bdf77ac53faa41dd7cb
mithun <mithunpaul08@gmail.com>
2020-05-29 00:36:39 -0700

    update to shell script

--- run_sandeep_code.sh
+++ run_sandeep_code.sh
@@ -1,5 +1,5 @@
  python run_classifier_ARC_DETAILED_sandeep.py \
- --task_name=fevercd --do_train=true --do_eval=true --data_dir=data/fever --data_dir_cross_domain="data/fnc" --vocab_file=trained_models/vocab.txt
- --bert_config_file=trained_models/bert_config.json --init_checkpoint=trained_models/bert_model.ckpt
- --max_seq_length=64  --train_batch_size=16 --learning_rate=1e-5 --num_train_epochs=5.0 --output_dir=output
+ --task_name=fevercd --do_train=true --do_eval=true --data_dir=data/fever --data_dir_cross_domain="data/fnc" --vocab_file=trained_models/vocab.txt \
+ --bert_config_file=trained_models/bert_config.json --init_checkpoint=trained_models/bert_model.ckpt \
+ --max_seq_length=64  --train_batch_size=16 --learning_rate=1e-5 --num_train_epochs=5.0 --output_dir=output \
  --num_train_epochs 1
