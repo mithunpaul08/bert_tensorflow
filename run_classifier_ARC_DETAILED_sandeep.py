2b1b354e58b66d69dc786a4b765e1fad39407371
mithun <mithunpaul08@gmail.com>
2020-05-29 01:28:54 -0700

    removing all comet

--- run_classifier_ARC_DETAILED_sandeep.py
+++ run_classifier_ARC_DETAILED_sandeep.py
@@ -17,7 +17,6 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-from comet_ml import Experiment,ExistingExperiment
 import collections
 import csv
 import os
