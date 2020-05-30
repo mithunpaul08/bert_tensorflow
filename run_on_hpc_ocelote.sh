6cbc43f1e8cdb039441eeff6b9c1b8e714b197ee
mithun <mithunpaul08@gmail.com>
2020-05-30 00:34:10 -0700

    back to python3

--- run_on_hpc_ocelote.sh
+++ run_on_hpc_ocelote.sh
@@ -8,7 +8,8 @@
 
 cd /home/u11/mithunpaul/
 module load cuda90/neuralnet/7/7.3.1.20
-module load python/2/2.7.14
+#module load python/2/2.7.14
+module load python/3/3.5.5
 
 #uncomment this if you don't want to reinstall venv- usually you just have to do this only once ever
 rm -rf sandeep_bert_venv
